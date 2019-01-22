d59cc911d55f0722fbac9ee8c86008fd64b0f37a
Dhiraj Chhabra <dhirajchhabra630@gmail.com>
2019-01-22 19:33:45 +0530

    added code for Jan 22, 2019

---
+++ Oct16_Basics/app/src/main/java/com/example/dhiraj/fbpage/WelcomePage.java
@@ -0,0 +1,15 @@
+package com.example.dhiraj.fbpage;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class WelcomePage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome_page);
+
+    }
+}
+
